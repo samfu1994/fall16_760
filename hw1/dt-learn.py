956a7e380cd1c9dcf426bed24a2c1e1a81d734ca
fuhao <gowithhao@yahoo.com>
2016-09-23 16:47:02 -0500

    numeric finish

---
+++ hw1/dt-learn.py
@@ -0,0 +1,335 @@
+import arff
+import math
+import csv
+import Queue
+
+
+train_data = [];
+test_data = [];
+label =[];
+attributeNum = 0;
+instanceNum = 0;
+nominal = {}
+stopThreshold = 2;
+MAX_INIT = -2147483648
+MIN_INIT = 2147483647
+median = 1
+def getPrediction(node):
+	p = 0
+	n = 0
+	currentSet = node.currentSet
+	for i in currentSet:
+		if label[i] == 0:
+			n += 1
+		else:
+			p += 1
+	if n > p:
+		return 0
+	elif n < p:
+		return 1
+	else:
+		return getPrediction(node.parent)
+
+
+class node:
+	def __init__(self, visited, currentSet, parent):
+		print ("")
+		print (len(currentSet))
+		self.isLeaf = 0
+		self.visited = visited
+		self.index = -1
+		self.threshold = -1
+		self.gainVal = 0
+		self.prediction = 0
+		self.parent = parent
+		self.currentSet = currentSet
+		self.child = []
+		if self.judgeStop(visited[:], currentSet):
+			self.isLeaf = 1
+			self.prediction = getPrediction(self)
+			return
+		gain = self.inforgain(visited[:], currentSet)
+
+		#if no information gain, then return
+		if gain <= 0:
+			self.isLeaf = 1
+			self.prediction = getPrediction(self)
+			return 
+		if nominal[self.index] != "NUMERIC":
+			# nominal
+			self.isNominal = 1
+		else:
+			self.isNominal = 0
+
+		self.visited.append(self.index);
+
+		if not self.isNominal:
+			[left, right] = self.getChild(currentSet, self.index, self.threshold)
+			self.child.append(node(self.visited[:], left, self))
+			self.child.append(node(self.visited[:], right, self))
+		else:
+			child = self.getChild(currentSet, self.index, self.threshold)
+		return
+
+	def predict(self, instance):
+		if self.isLeaf:
+			# print ("prediction is " + str(self.index))
+			return self.prediction
+		else:
+			branch = self.predict_helper(instance)
+			# print ("I am " + str(self.index) + "  go to " + str(branch))
+			return self.child[branch].predict(instance)
+	def predict_helper(self, instance):
+		if instance[self.index] > self.threshold:
+			return 1
+		else:
+			return 0
+	def getChild(self, currentSet, index, threshold):
+		if not self.isNominal:
+			left = []
+			right = []
+			for i in currentSet:
+				if train_data[i][index] > threshold:
+					right.append(i)
+				else:
+					left.append(i)
+			return [left, right]
+		else:
+			return 
+	def inforgain(self, visited, currentSet):
+		max_val = MAX_INIT
+		index = -1
+		for attribute_index in range(attributeNum):
+			if visited.count(attribute_index) != 0:
+				continue
+			if nominal[attribute_index] == "NUMERIC":
+				vec = self.helper_numeric(attribute_index, currentSet[:])
+				val = vec[0]
+				mid = vec[1]
+			else:
+				val = self.helper_nominal(attribute_index, currentSet[:])
+			# print "		finish attribute " + str(attribute_index)
+			#val is negative
+			if val > max_val:
+				max_val = val
+				index = attribute_index
+
+		p = 0
+		n = 0
+		for i in currentSet:
+			if label[i] == 1:
+				p += 1
+			else:
+				n += 1
+		pn = p + n + 0.0
+		if p == pn or n == pn:
+			parent_entropy = 0
+		else:
+			parent_entropy = -(p / pn * math.log(p / pn)) - (n / pn * math.log(n / pn))
+		gain = parent_entropy + max_val
+		if gain <= 0:
+			return gain
+
+		visited.append(index)
+		print ("select  " + str(index) )
+		print ("threshold : " + str(mid))
+		self.index = index
+		self.threshold = mid
+		return gain
+	
+
+
+	def judgeStop(self, visited, currentSet):
+		#no more split candidate
+		if len(visited) == attributeNum:
+			return 1
+
+		#have less than m instances
+		if len(currentSet) < stopThreshold:
+			print "have less than m instances"
+			return 1
+
+		#has only one class
+		l = []
+		for i in currentSet:
+			if l.count(label[i]) != 0:
+				l.append(label[i])
+		if len(l) == 1:
+			print "has only one class"
+			return 1
+
+		return 0
+
+	def numeric_info(self, vec, mid, currentSet):
+		length = len(vec)
+		falseNeg = 0
+		truePos = 0
+		falsePos = 0
+		trueNeg = 0
+		for i in range(length):
+			if vec[i] <= mid:
+				if label[currentSet[i]] == 0:
+					trueNeg += 1
+				else:
+					falseNeg += 1
+			else:
+				if label[currentSet[i]] == 0:
+					falsePos += 1
+				else:
+					truePos += 1
+		#plogp calculate the max
+		pNeg = (trueNeg + falseNeg + 0.0) / length
+		pPos = 1 - pNeg
+
+		if pNeg == 1 or pPos == 1:
+			return 0
+
+
+		acc = 0.0
+		t1 = trueNeg / (trueNeg + falseNeg + 0.0)
+		t2 = 1 - t1
+		if t1 != 1 and t1 != 0:
+			acc += pNeg * ( t1 * math.log(t1) + t2 * math.log(t2) )
+
+
+		t1 = truePos / (truePos + falsePos + 0.0)
+		t2 = 1 - t1
+		if t1 != 1 and t1 != 0:
+			acc += pPos * ( t1 * math.log(t1) + t2 * math.log(t2) )
+		# -1 ~ 0
+		return acc
+	def helper_numeric(self, index, currentSet):
+		#find the best split in a particular feature
+
+		vec = []
+		for instance_index in currentSet:
+			vec.append(train_data[instance_index][index])
+		l = len(currentSet)
+		min_val = MIN_INIT
+		max_val = MAX_INIT
+		max_entropy = -1
+
+
+		if median:
+			currentSet.sort()
+			tmp = currentSet[len(currentSet) / 2]
+			mid = train_data[tmp][index]
+			# print mid
+		else:
+			for i in currentSet:
+				cur = train_data[i][index]
+				if cur > max_val:
+					max_val = cur
+				if cur < min_val:
+					min_val = cur
+			mid = (max_val + min_val) / 2
+		max_entropy = self.numeric_info(vec, mid, currentSet)
+
+
+		# for i in range(l - 1):
+		# 		instance_index = currentSet[i];
+		# 		second = currentSet[i + 1]
+		# 		# print str(instance_index) + "   " + str(second)
+		# 		mid = (train_data[instance_index][index] + train_data[second][index]) / 2
+		# 		max_entropy = max(max_entropy, self.numeric_info(vec, mid, currentSet) )
+
+		return [max_entropy, mid]
+	def helper_nominal(self, index, currentSet):
+		return
+	
+def check(q):
+	if q.empty():
+		return
+	qq = Queue.Queue()
+
+	s = ""
+	while not q.empty():
+		tmp = q.get()
+		l = len(tmp.child)
+		s += str(tmp.index)
+		s += "    "
+		for i in range(l):
+			qq.put(tmp.child[i])
+	print "      " + s
+	check(qq)
+
+def main():
+	global train_data, test_data, attributeNum, instanceNum, nominal, label
+
+	train_data = arff.load(open('data/diabetes_train.arff'))
+	test_data = arff.load(open('data/diabetes_test.arff'))
+
+	attributeNum = len(train_data["attributes"]) - 1
+	if attributeNum < 0:
+		return
+
+	#nominal store the nominal attribute name and value 
+	count = 0
+	for ele in train_data["attributes"]:
+		if ele[0] != "class": 
+			nominal[count] = ele[1]
+		count += 1
+	map_label = {}
+	count = 0
+	for ele in train_data["data"]:
+		if ele[attributeNum] not in map_label:
+			map_label[ele[attributeNum]] = count
+			count += 1
+		label.append(map_label[ele[attributeNum]])
+	# print label
+	visited = []
+	instanceNum = len(train_data["data"])
+	currentSet = [i for i in range(instanceNum)]
+
+	train_data = train_data["data"]
+	with open("data/c.csv", 'wb') as csvfile:
+		writer = csv.writer(csvfile);
+		for i in train_data:
+			writer.writerows([i])
+	test_data = test_data["data"]
+	root = node(visited, currentSet, 0)
+
+	print ("-------------------------------------------------------------------------------------------")
+
+	q = Queue.Queue()
+	q.put(root)
+	check(q)
+
+
+	train_label = []
+	predict_label = []
+
+	for ele in train_data:
+		train_label.append(map_label[ele[attributeNum]])
+		predict_label.append(root.predict(ele))
+
+	train_num = len(train_label)
+	correct = 0
+	for i in range(train_num):
+		# print str(train_label[i]) + "    " +  str(predict_label[i])
+		if train_label[i] == predict_label[i]:
+			correct += 1
+	correct += 0.0
+	print ("correct rate:" + str(correct / train_num) )
+
+
+
+	test_label = []
+	predict_label = []
+	for ele in test_data:
+		test_label.append(map_label[ele[attributeNum]])
+		predict_label.append(root.predict(ele))
+
+	test_num = len(test_label)
+	correct = 0
+	for i in range(test_num):
+		# print str(test_label[i]) + "    " +  str(predict_label[i])
+		if test_label[i] == predict_label[i]:
+			correct += 1
+		print str(test_label[i]) +"      "  + str(predict_label[i])
+	correct += 0.0
+	print ("correct rate:" + str(correct / test_num) )
+
+
+if __name__ == "__main__":
+	main()
